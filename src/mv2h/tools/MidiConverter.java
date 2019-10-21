9e29fe22d27521ef031ea2e9d77b68e845ee654b
Andrew McLeod <apmcleod@gmail.com>
2019-10-21 16:28:51 +0900

    Added new xml and midi classes

---
+++ src/mv2h/tools/MidiConverter.java
@@ -0,0 +1,12 @@
+package mv2h.tools;
+
+import java.io.InputStream;
+
+public class MidiConverter extends Converter {
+
+	public MidiConverter(InputStream stream) {
+		super(stream);
+		// TODO Auto-generated constructor stub
+	}
+
+}
